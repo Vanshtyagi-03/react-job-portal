5527f889e77319725f4dd09dd93be0c183156173
Vansh Tyagi <vanshtyagi913@gmail.com>
2026-03-05 12:19:15 +0530

    Uncomment ProtectedRoute component code
    Restore ProtectedRoute component implementation.

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,21 +1,3 @@
-// import { Navigate } from "react-router-dom";
-
-// function ProtectedRoute({ children }) {
-//   const role = localStorage.getItem("role");
-
-//   if (!role) {
-//     return <Navigate to="/login" />;
-//   }
-
-//   if (role !== "admin") {
-//     return <Navigate to="/" />;
-//   }
-
-//   return children;
-// }
-
-// export default ProtectedRoute;
-
 import { Navigate } from "react-router-dom";
 
 function ProtectedRoute({ children }) {
