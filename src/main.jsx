1c91879a0ae0f0fd0bf6a4fac7803f92a67f4d86
Vansh Tyagi <vanshtyagi913@gmail.com>
2026-03-05 12:23:07 +0530

    Clean up main.jsx by removing commented code
    Removed commented-out import statements and rendering code.

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,3 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import App from './App.jsx'
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import 'animate.css';
-
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
